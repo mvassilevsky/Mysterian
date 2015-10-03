bc4ce050112ce49247c355c3cf74744d2e194e94
samiranne <samira.nedungadi@gmail.com>
2015-10-03 00:10:36 -0700

    Refactoring characters_controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -34,18 +34,18 @@ class CharactersController < ApplicationController
 
     respond_to do |format|
       if @character.save
-        if @player.nil? and !params[:player_email].blank?
-          format.html { redirect_to game_path(@character.game_id),
-                        notice: 'Character was successfully created, but ' +
-                                'there is no user with that email address.' }
-        elsif !params[:character][:player_email].blank?
-          game_user = GameUser.find_by(user_id: @character.user.id,
-                                       game_id: @character.game.id)
-          GameUser.create(user_id: @player.id,
-                          game_id: @character.game.id) unless game_user
-          UserMailer.invite_email(@player, @character.game, @character).deliver
-          format.html { redirect_to game_path(@character.game_id),
-                        notice: 'Character was successfully created.' }
+        if !params[:character][:player_email].blank?
+          if @player.nil?
+              format.html { redirect_to game_path(@character.game_id),
+                            notice: 'Character was successfully created, but ' +
+                                    'there is no user with that email address.' }
+          else  
+            GameUser.where(user_id: @character.user.id,
+                           game_id: @character.game.id).first_or_create
+            UserMailer.invite_email(@player, @character.game, @character).deliver
+            format.html { redirect_to game_path(@character.game_id),
+                          notice: 'Character was successfully created.' }
+          end
         else
           format.html { redirect_to game_path(@character.game_id),
                         notice: 'Character was successfully created.' }
@@ -56,21 +56,6 @@ class CharactersController < ApplicationController
         format.json { render json: @character.errors,
                       status: :unprocessable_entity }
       end
-
-      # begin
-      #   Character.transaction do
-      #     @character.save!
-      #     # byebug
-      #     GameUser.create!(game: @character.game, user: @character.user)
-      #   end
-      #   format.html { redirect_to game_path(@character.game_id), notice: 'Character was successfully created.' }
-      #   format.json { render :show, status: :created, location: @character }
-      # rescue
-      #   @game = Game.find(@character.game_id)
-      #   @characters = @game.characters
-      #   format.html { render "games/show", notice: 'Error creating character.' }
-      #   format.json { render json: @character.errors, status: :unprocessable_entity }
-      # end
     end
   end
 
@@ -78,34 +63,31 @@ class CharactersController < ApplicationController
   # PATCH/PUT /games/:game_id/characters/1.json
   def update
     @player = User.find_by(params[:character][:player_email])
-    new_user_assigned = false
-
-    #change this when we have registration working
-    unless @player.nil?
+    
+    if @player.nil?
+      @character.user_id = nil  
+      new_user_assigned = false 
+    else
+      @character.user_id = @player.id 
       new_user_assigned = true
-      @character.user_id = @player.id
     end
 
     respond_to do |format|
-      if @character.update(character_params)
-        if @player.nil? and !params[:character][:player_email].blank?
-          format.html { redirect_to game_path(@character.game_id),
-                        notice: 'Character was successfully updated, but ' +
-                        'there is no user with that email address.' }
-        else
-          if new_user_assigned
-            game_user = GameUser.find_by(user_id: @character.user.id,
-                                         game_id: @character.game.id)
-            GameUser.create(user_id: @character.user.id,
-                            game_id: @character.game.id) unless game_user
-            UserMailer.invite_email(@player, @character.game,
-                                    @character).deliver
-          end
+      if @character.update(character_params)          
+        if new_user_assigned
+          GameUser.where(user_id: @character.user.id,
+                         game_id: @character.game.id).first_or_create
+          UserMailer.invite_email(@player, @character.game,
+                                  @character).deliver
           format.html { redirect_to game_path(@character.game_id),
                         notice: 'Character was successfully updated.' }
+        else
+          format.html { redirect_to game_path(@character.game_id),
+                      notice: 'Character was successfully updated, but ' +
+                      'there is no user with that email address.' }
         end
         format.json { render :show, status: :ok,
-                      location: game_path(@character.game_id) }
+                    location: game_path(@character.game_id) }
       else
         format.html { render :edit }
         format.json { render json: @character.errors,
@@ -117,8 +99,6 @@ class CharactersController < ApplicationController
   # DELETE /games/:game_id/characters/1
   # DELETE /games/:game_id/characters/1.json
   def destroy
-    @game_user = GameUser.find_by(game: @character.game, user: @character.user)
-    @game_user.destroy unless @game_user.nil?
     @character.destroy
     respond_to do |format|
       format.html { redirect_to game_path(@character.game_id),
